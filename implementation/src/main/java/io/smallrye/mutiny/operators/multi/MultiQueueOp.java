2575842ffdae8cb5e0df46849b3ea672f1d6f227
Ben Bemis <bbemis@alumni.purdue.edu>
2022-02-13 15:17:34 -0600

    setup subscription & pass items downstream

--- implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiQueueOp.java
+++ implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiQueueOp.java
@@ -18,6 +18,15 @@ import org.reactivestreams.Subscription;
  * @param <T> the type of item
  */
 public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Subscriber<T> {
+    /**
+     * The upstreamSubscription is used to control the flow of messages between it's Publisher (the upstream) and it's Subscriber (MultiQueueOp).
+     */
+    private Subscription upstreamSubscription;
+
+    /**
+     *  The multiQueueOpSubscription is used to control the flow of messages between it's Publisher (MultiQueueOp) and the MultiQueueOp's downstream subscriber.
+     */
+    private MultiQueueOpSubscription<T> multiQueueOpSubscription;
 
 
     /**
@@ -33,8 +42,15 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
 
     @Override
     public void subscribe(MultiSubscriber<? super T> downstreamSubscriber) {
-//        super.subscribe(downstreamSubscriber);
         // this is called when the downstream subscribes to the MultiQueueOp
+
+        // The downstream of the MultiQueueOp subscribes to the MultiQueueOpSubscription
+        MultiQueueOpSubscription<T> multiQueueOpSubscription = new MultiQueueOpSubscription<>(downstreamSubscriber, this);
+        downstreamSubscriber.onSubscribe(multiQueueOpSubscription);
+        this.multiQueueOpSubscription = multiQueueOpSubscription;
+
+        // subscribe to the upstream of the MultiQueueOp
+        this.upstream().subscribe().withSubscriber(this);
     }
 
     /**
@@ -47,13 +63,13 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
     public void onSubscribe(Subscription subscription) {
         // is called before processing starts when MultiQueueOp subscribes to the upstream.
         // the subscription is used to control the flow of messages between the Subscriber (MultiQueueOp) and the Publisher (the upstream).
-
+        this.upstreamSubscription = subscription;
     }
 
     @Override
     public void onNext(T t) {
         // called when the publisher publishes a new message
-
+        this.multiQueueOpSubscription.send(t);
     }
 
     @Override
@@ -68,18 +84,34 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
 
 
     /**
-     * The {@link MultiQueueOp2.QueueSubscription} is used to control the flow of messages between the
+     * The {@link MultiQueueOpSubscription} is used to control the flow of messages between the
      * Publisher (MultiQueueOp) and the MultiQueueOp's downstream subscriber. This class should be instantiated and
      * passed to the downstream subscriber through the {@link io.smallrye.mutiny.subscription.MultiSubscriber}'s
      * onSubscribe method.
      *
      * @param <T> the type of item
      */
-    static final class QueueSubscription<T> implements Subscription {
+    static final class MultiQueueOpSubscription<T> implements Subscription {
+
+        private final MultiQueueOp<T> multiQueueOp;
+
+        /**
+         * The MultiQueueOp's downstream subscriber.
+         */
+        private final MultiSubscriber<? super T> downstreamSubscriber;
+
+        public MultiQueueOpSubscription(MultiSubscriber<? super T> downstreamSubscriber, MultiQueueOp<T> multiQueueOp) {
+            this.multiQueueOp = multiQueueOp;
+            this.downstreamSubscriber = downstreamSubscriber;
+        }
 
         @Override
         public void request(long l) {
+            this.multiQueueOp.upstreamSubscription.request(l);
+        }
 
+        public void send(T item) {
+            downstreamSubscriber.onItem(item);
         }
 
         @Override
