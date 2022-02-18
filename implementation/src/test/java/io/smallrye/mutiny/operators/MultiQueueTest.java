30ef6014a3e36f33ff5a9b7493648d695904a1c9
Ben Bemis <bbemis@alumni.purdue.edu>
2022-02-17 22:14:07 -0600

    testQueueingWithFullQueue

--- implementation/src/test/java/io/smallrye/mutiny/operators/MultiQueueTest.java
+++ implementation/src/test/java/io/smallrye/mutiny/operators/MultiQueueTest.java
@@ -2,11 +2,9 @@ package io.smallrye.mutiny.operators;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.helpers.test.AssertSubscriber;
-import io.smallrye.mutiny.helpers.test.AssertionHelper;
 import io.smallrye.mutiny.operators.multi.MultiQueueOp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Subscriber;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -16,9 +14,7 @@ public class MultiQueueTest {
     // TODO Implement tests
     @Test
     public void testQueueingWithResultsAndCompletion() {
-        AtomicInteger count = new AtomicInteger();
-        Multi<Integer> multi = Multi.createFrom().deferred(() -> Multi.createFrom().items(count.incrementAndGet(),
-                        count.incrementAndGet()))
+        Multi<Integer> multi = Multi.createFrom().items(1,2)
                 .plug((m) -> new MultiQueueOp<>(m, 1));
         multi.subscribe().withSubscriber(AssertSubscriber.create())
                 .assertSubscribed()
@@ -59,9 +55,25 @@ public class MultiQueueTest {
         Assertions.assertNotNull(failure);
  }
 
+    @Test
+    void testQueueingWithFullQueue() {
+        AtomicInteger count = new AtomicInteger();
+        Multi<Integer> multi = Multi.createBy()
+                .repeating().supplier(count::getAndIncrement).atMost(5)
+                .plug((m) -> new MultiQueueOp<>(m, 1));
+
+        AssertSubscriber<Integer> subscriber = multi.subscribe().withSubscriber(AssertSubscriber.create());
+
+        subscriber.assertSubscribed()
+                .request(1)
+                .awaitItems(1);
+
+        // Even though we have only requested 1 item, the second item should have been queued up
+        // So the count should be 2. But only the second item because we only queued one.
+        Assertions.assertEquals(2, count.get());
+
+        subscriber.cancel();
+     }
+
     //TODO can I test for race conditions?
-
-
-    // TODO test examples from MultiCacheTEst
-    //testCachingWithDeferredResult
 }
