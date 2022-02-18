608e6d260c7841f5b0eb35d7132c9214fbb182da
Ben Bemis <bbemis@alumni.purdue.edu>
2022-02-17 21:45:02 -0600

    testQueueingWithFailure passes

--- implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiQueueOp.java
+++ implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiQueueOp.java
@@ -1,9 +1,14 @@
 package io.smallrye.mutiny.operators.multi;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.helpers.Subscriptions;
 import io.smallrye.mutiny.subscription.MultiSubscriber;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 // TODO bbemis it might be a good exercise to implement a simpler MultiOperator first that just passes everything along
 // nothing fancy
@@ -19,6 +24,11 @@ import org.reactivestreams.Subscription;
  */
 public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Subscriber<T> {
     /**
+     * Stores a limited number of events at a time. Queue follows FIFO
+     */
+    private final BlockingQueue<T> eventQueue;
+
+    /**
      * The upstreamSubscription is used to control the flow of messages between it's Publisher (the upstream) and it's Subscriber (MultiQueueOp).
      */
     private Subscription upstreamSubscription;
@@ -33,6 +43,7 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
      */
     private boolean upstreamIsComplete; // TODO: https://www.geeksforgeeks.org/volatile-keyword-in-java/#:~:text=For%20Java%2C%20%E2%80%9Cvolatile%E2%80%9D%20tells,scope%20of%20the%20program%20itself.
 
+    private Throwable failure;
 
     /**
      * =========================================================================
@@ -41,8 +52,9 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
      * =========================================================================
      */
 
-    public MultiQueueOp(Multi<? extends T> upstream) {
+    public MultiQueueOp(Multi<? extends T> upstream, final int maxQueueSize) {
         super(upstream);
+        this.eventQueue = new LinkedBlockingQueue<>(maxQueueSize);
     }
 
     @Override
@@ -58,6 +70,10 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
         this.upstream().subscribe().withSubscriber(this);
     }
 
+    public void requestToRefillQueue() {
+        this.upstreamSubscription.request(this.eventQueue.remainingCapacity());
+    }
+
     /**
      * =========================================================================
      * =================== Overriding Subscriber Interface  ====================
@@ -69,17 +85,28 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
         // is called before processing starts when MultiQueueOp subscribes to the upstream.
         // the subscription is used to control the flow of messages between the Subscriber (MultiQueueOp) and the Publisher (the upstream).
         this.upstreamSubscription = subscription;
+
+        this.requestToRefillQueue();
     }
 
     @Override
     public void onNext(T t) {
+        System.out.println("MultiQueueOp#onNext");
         // called when the publisher publishes a new message
-        this.multiQueueOpSubscription.send(t);
+        this.eventQueue.add(t);
+
+        this.multiQueueOpSubscription.drainQueue();
     }
 
     @Override
     public void onError(Throwable throwable) {
+        if (this.upstreamIsComplete) {
+            return;
+        }
 
+        this.failure = throwable;
+        this.upstreamIsComplete = true;
+        this.multiQueueOpSubscription.sendCompletion();
     }
 
     @Override
@@ -108,29 +135,70 @@ public class MultiQueueOp<T> extends AbstractMultiOperator<T, T> implements  Sub
          */
         private final MultiSubscriber<? super T> downstreamSubscriber;
 
+        private final AtomicLong requestedItems = new AtomicLong();
+
         public MultiQueueOpSubscription(MultiSubscriber<? super T> downstreamSubscriber, MultiQueueOp<T> multiQueueOp) {
             this.multiQueueOp = multiQueueOp;
             this.downstreamSubscriber = downstreamSubscriber;
         }
 
         @Override
-        public void request(long l) {
-            this.multiQueueOp.upstreamSubscription.request(l);
+        public void request(long numOfItems) {
+            if (numOfItems <= 0 || this.multiQueueOp.failure != null) return;
+
+            Subscriptions.add(this.requestedItems, numOfItems);
+            drainQueue();
         }
 
         public void sendCompletion() {
+            if (this.multiQueueOp.failure != null) {
+                this.downstreamSubscriber.onError(this.multiQueueOp.failure);
+                return;
+            }
+
             if (this.multiQueueOp.upstreamIsComplete) {
                 System.out.println("MultiQueueOpSubscription#sendCompletion completion");
                 this.downstreamSubscriber.onCompletion();
             }
         }
 
-        public void send(T item) {
-            System.out.println("MultiQueueOpSubscription#send start");
+        public void drainQueue() {
+            System.out.println("MultiQueueOpSubscription#drainQueue");
 
-            this.downstreamSubscriber.onItem(item);
+            // if the upstream is complete and there are no events remaining
+            if(this.multiQueueOp.upstreamIsComplete && this.multiQueueOp.eventQueue.remainingCapacity() < 1) {
+                if (this.multiQueueOp.failure != null) {
+                    // pass the failure from the MultiQueueOp to the MultiQueueOp's downstream subscriber
+                    this.downstreamSubscriber.onError(this.multiQueueOp.failure);
+                } else {
+                    this.downstreamSubscriber.onCompletion();
+                }
+                return;
+            }
+            System.out.println("Check 1");
 
-            System.out.println("MultiQueueOpSubscription#send end");
+            long consumerRequested = this.requestedItems.get();
+            System.out.println("req; " + consumerRequested + " cap: " + this.multiQueueOp.eventQueue.remainingCapacity());
+            if (consumerRequested == Long.MIN_VALUE) { // if cancelled
+                return;
+            } else if(consumerRequested > 0L && this.multiQueueOp.eventQueue.remainingCapacity() < this.multiQueueOp.eventQueue.size()) {
+                System.out.println("MulitQueueOpSubscription#drainQueue request > 0 && queueRemainingCapacity > 0");
+                try {
+                    T event = this.multiQueueOp.eventQueue.take();
+                    this.downstreamSubscriber.onItem(event);
+                } catch (InterruptedException e) {
+                    // tell the MultiQueueOp that an error occurred. This should clean things up properly between the
+                    // upstream and downstream in theory.
+                    System.out.println("Interrupted exception");
+                    this.multiQueueOp.onError(e); //TODO could this end up looping if onError calls drain?
+                } finally {
+                    System.out.println("request fulfilled, remaining QueueSize" + this.multiQueueOp.eventQueue.remainingCapacity());
+                    Subscriptions.subtract(this.requestedItems, 1);
+                    this.multiQueueOp.requestToRefillQueue();
+//                    continue;
+                }
+            }
+
         }
 
         @Override
--- implementation/src/test/java/io/smallrye/mutiny/operators/MultiQueueTest.java
+++ implementation/src/test/java/io/smallrye/mutiny/operators/MultiQueueTest.java
@@ -2,9 +2,13 @@ package io.smallrye.mutiny.operators;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.helpers.test.AssertSubscriber;
+import io.smallrye.mutiny.helpers.test.AssertionHelper;
 import io.smallrye.mutiny.operators.multi.MultiQueueOp;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiQueueTest {
@@ -15,40 +19,45 @@ public class MultiQueueTest {
         AtomicInteger count = new AtomicInteger();
         Multi<Integer> multi = Multi.createFrom().deferred(() -> Multi.createFrom().items(count.incrementAndGet(),
                         count.incrementAndGet()))
-                        .queue(1);
-        multi.subscribe().withSubscriber(AssertSubscriber.create(2))
-                .assertCompleted()
-                .assertItems(1, 2);
-
-        multi.subscribe().withSubscriber(AssertSubscriber.create(Long.MAX_VALUE))
-                .assertCompleted()
-                .assertItems(1, 2);
-    }
-
-    @Test
-    public void testQueueingWithResultsAndCompletion2() {
-        AtomicInteger count = new AtomicInteger();
-        Multi<Integer> multi = Multi.createFrom().deferred(() -> Multi.createFrom().items(count.incrementAndGet(),
-                        count.incrementAndGet()))
-                .plug(MultiQueueOp::new);
+                .plug((m) -> new MultiQueueOp<>(m, 1));
         multi.subscribe().withSubscriber(AssertSubscriber.create())
                 .assertSubscribed()
                 .request(2)
                 .awaitItems(2)
                 .assertCompleted()
                 .assertItems(1, 2);
+    }
 
-        //TODO not really sure if we need to support multiple subscribers? hmm
-//        multi.subscribe().withSubscriber(AssertSubscriber.create(Long.MAX_VALUE))
-//                .assertCompleted()
-//                .assertItems(1, 2);
+    @Test
+    public void testQueueingWithFailure() {
+        AtomicInteger count = new AtomicInteger();
+        Multi<Integer> multi = Multi.createFrom().<Integer> emitter(emitter -> emitter.emit(count.incrementAndGet())
+                        .emit(count.incrementAndGet())
+                        .fail(new IOException("boom-" + count.incrementAndGet())))
+                .plug((m) -> new MultiQueueOp<>(m, 1));
+
+        multi.subscribe().withSubscriber(AssertSubscriber.create(2))
+                .awaitItems(2)
+                .assertItems(1, 2)
+                .awaitFailure((throwable) -> new AssertionError("test"));
+    }
+
+    @Test
+    public void testQueueingFailsOnSecondSubscriber() {
+        AtomicInteger count = new AtomicInteger();
+        Multi<Integer> multi = Multi.createFrom().deferred(() -> Multi.createFrom().items(count.incrementAndGet(),
+                        count.incrementAndGet()))
+                .plug((m) -> new MultiQueueOp<>(m, 1));
+
+        multi.subscribe().withSubscriber(AssertSubscriber.create())
+                .assertSubscribed();
+
+        Assertions.assertThrows(Exception.class, () -> multi.subscribe().withSubscriber(AssertSubscriber.create()).assertSubscribed());
     }
 
     //TODO can I test for race conditions?
 
 
     // TODO test examples from MultiCacheTEst
-    //testCachingWithResultsAndCompletion
-    //testCachingWithFailure
     //testCachingWithDeferredResult
 }
